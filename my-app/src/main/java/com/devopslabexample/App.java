6a16e23df7b254cdac181d81f51e1939f337bfda
cheefcolt <gare22ise@cmrit.ac.in>
2025-04-17 09:28:31 +0530

    My first azure pipeline

---
+++ my-app/src/main/java/com/devopslabexample/App.java
@@ -0,0 +1,13 @@
+package com.devopslabexample;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
